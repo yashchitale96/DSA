4f86d6fc69d0fc366d3a26814fcf615425fd4da8
yashchitale96 <114438423+yashchitale96@users.noreply.github.com>
2023-08-16 14:23:00 +0530

    Create 01char.cpp

---
+++ char array and string/01char.cpp
@@ -0,0 +1,37 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+
+/* \0 null pointer is act as terminater in char array and in string */
+
+int main()
+{
+    /* REPRESENTATION OF CHAR ARRAY & STRING */
+    
+    char a = {'a'};
+    
+    char ch[4] = {'y','a','s','h'};
+
+    string name = "chitale";
+
+    /* INPUT OUTPUT IN CHAR ARRAY & STRING */
+
+    char x;
+    cout << "Enter the single character: ";
+    cin>> x;
+    cout << "Character is: " << x << endl;
+
+    char y[4];
+    cout << "Enter the character in char array: ";
+    cin>> y;
+    cout << "Character is: " << y << endl;
+
+    string z;
+    cout << "Enter the character in string: ";
+    cin>> z;
+    cout << "Character is: " << z << endl;
+
+
+
+
+}
